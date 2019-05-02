0faf652ab8946b080deaf70a5bc21d5aefc7fa5d
Andy Weber <andrew.weber@opower.com>
2019-05-02 17:35:50 -0400

    4 space tabs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="home">
-    <h1>Movie List!</h1>
-    <p>These were the top-rated films of the 20th Century.</p>
+    <div class="home">
+        <h1>Movie List!</h1>
+        <p>These were the top-rated films of the 20th Century.</p>
 
-    <ul class="movie-list">
-      <li v-for="movie in movies.results">
-        <div>
-          <div class="left">
-            <img
-              :src="movie.poster_path"
-            />
-          </div>
-          <div class="right"
-            :style="{'background-image': 'url(' + movie.backdrop_path + ')'}"
-          >
-            <div>{{movie.title}} {{movie.release_date}}</div>
-            <div>{{movie.genres}}</div>
-            <div>{{movie.overview}}</div>
-            <div>{{movie.backdrop_path}}</div>
-            <p class="rating">
-              <span>Viewer Rating:</span>{{movie.vote_average}} / 10
-              <button v-if="!isFavorite(movie)" class="favorite" @click="addToFavorites(movie)">Add to favorites</button>
-              <span v-else>Favorite! <a @click="true" class="remove_favorite">(UnFavorite)</a></span>
-            </p>
-          </div>
-        </div>
-      </li>
-    </ul>
-  </div>
+        <ul class="movie-list">
+        <li v-for="movie in movies.results">
+            <div>
+            <div class="left">
+                <img
+                :src="movie.poster_path"
+                />
+            </div>
+            <div class="right"
+                :style="{'background-image': 'url(' + movie.backdrop_path + ')'}"
+            >
+                <div>{{movie.title}} {{movie.release_date}}</div>
+                <div>{{movie.genres}}</div>
+                <div>{{movie.overview}}</div>
+                <div>{{movie.backdrop_path}}</div>
+                <p class="rating">
+                <span>Viewer Rating:</span>{{movie.vote_average}} / 10
+                <button v-if="!isFavorite(movie)" class="favorite" @click="addToFavorites(movie)">Add to favorites</button>
+                <span v-else>Favorite! <a @click="true" class="remove_favorite">(UnFavorite)</a></span>
+                </p>
+            </div>
+            </div>
+        </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -36,35 +36,35 @@ import FavoriteButton from "@/components/FavoriteButton.vue";
 import { mapState, mapActions } from 'vuex';
 
 export default {
-  name: "home",
-  components: {
-    FavoriteButton
-  },
-  computed: mapState({
-    movies: state => state.movies,
-    favorites: state => state.favorites
-  }),
-  methods: {
-    ...mapActions(['addFavorite']),
-    addToFavorites: function ( movie ) {
-      console.log('adding: ' + movie);
-      this.addFavorite(movie);
+    name: "home",
+    components: {
+        FavoriteButton
     },
-    isFavorite: function ( movie ) {
-      return this.favorites.filter(fave => fave.id == movie.id).length > 0;
+    computed: mapState({
+        movies: state => state.movies,
+        favorites: state => state.favorites
+    }),
+    methods: {
+        ...mapActions(['addFavorite']),
+        addToFavorites: function ( movie ) {
+            console.log('adding: ' + movie);
+            this.addFavorite(movie);
+        },
+        isFavorite: function ( movie ) {
+            return this.favorites.filter(fave => fave.id == movie.id).length > 0;
+        }
     }
-  }
 };
 </script>
 
 <style scoped>
 .left {
-  display: inline-block;
-  width: 20%;
-  height: 120px;
+    display: inline-block;
+    width: 20%;
+height: 120px;
 }
 .right {
-  display: inline-block;
-  width: 80%;
+    display: inline-block;
+    width: 80%;
 }
 </style>
--- tests/unit/favoriteButton.spec.js
+++ tests/unit/favoriteButton.spec.js
@@ -9,50 +9,50 @@ const localVue = createLocalVue();
 localVue.use(Vuex);
 
 describe("FavoriteButton.vue", () => {
-  let store;
-  let propsData;
+    let store;
+    let propsData;
 
-  beforeEach(() => {
-    store = dataStore;
+    beforeEach(() => {
+        store = dataStore;
 
-    propsData = {
-      movie: {
-        vote_count: 3676,
-        id: 78,
-        video: false,
-        vote_average: 7.9,
-        title: "Blade Runner",
-        popularity: 105.797529,
-        poster_path:
-          "https://image.tmdb.org/t/p/w185_and_h278_bestv2/p64TtbZGCElxQHpAMWmDHkWJlH2.jpg",
-        original_language: "en",
-        original_title: "Blade Runner",
-        backdrop_path:
-          "https://image.tmdb.org/t/p/w533_and_h300_bestv2/k36huckDH0v3LP1zo7maFt3mJC0.jpg",
-        adult: false,
-        overview:
-          "In the smog-choked dystopian Los Angeles of 2019, blade runner Rick Deckard is called out of retirement to terminate a quartet of replicants who have escaped to Earth seeking their creator for a way to extend their short life spans.",
-        release_date: "1982-06-25",
-        genres: ["Science Fiction", "Drama", "Thriller"]
-      }
-    };
-  });
-
-  it("adds favorite if movie is not yet favorited when clicked", () => {
-    const wrapper = shallowMount(FavoriteButton, {
-      propsData,
-      store,
-      localVue
+        propsData = {
+            movie: {
+                vote_count: 3676,
+                id: 78,
+                video: false,
+                vote_average: 7.9,
+                title: "Blade Runner",
+                popularity: 105.797529,
+                poster_path:
+                    "https://image.tmdb.org/t/p/w185_and_h278_bestv2/p64TtbZGCElxQHpAMWmDHkWJlH2.jpg",
+                original_language: "en",
+                original_title: "Blade Runner",
+                backdrop_path:
+                    "https://image.tmdb.org/t/p/w533_and_h300_bestv2/k36huckDH0v3LP1zo7maFt3mJC0.jpg",
+                adult: false,
+                overview:
+                    "In the smog-choked dystopian Los Angeles of 2019, blade runner Rick Deckard is called out of retirement to terminate a quartet of replicants who have escaped to Earth seeking their creator for a way to extend their short life spans.",
+                release_date: "1982-06-25",
+                genres: ["Science Fiction", "Drama", "Thriller"]
+            }
+        };
     });
-    // TODO: Fill in test for adding favorite.
-  });
 
-  it("removes a favorite when the movie is already in the favorites Array", () => {
-    const wrapper = shallowMount(FavoriteButton, {
-      propsData,
-      store,
-      localVue
+    it("adds favorite if movie is not yet favorited when clicked", () => {
+        const wrapper = shallowMount(FavoriteButton, {
+            propsData,
+            store,
+            localVue
+        });
+        // TODO: Fill in test for adding favorite.
     });
-    // TODO: Fill in test for removing favorite.
-  });
+
+    it("removes a favorite when the movie is already in the favorites Array", () => {
+        const wrapper = shallowMount(FavoriteButton, {
+            propsData,
+            store,
+            localVue
+        });
+        // TODO: Fill in test for removing favorite.
+    });
 });
