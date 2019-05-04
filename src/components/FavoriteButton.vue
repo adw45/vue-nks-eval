5346f3074d3b85d9dc34e435ede778d8c74dec3a
adw45 <weber.andrew1@gmail.com>
2019-05-03 21:36:39 -0400

    implement favorite button component (#3)
    * implement favorite button component
    
    * code cleanup

--- src/components/FavoriteButton.vue
+++ src/components/FavoriteButton.vue
@@ -1,15 +1,39 @@
 <template>
-  <button class="favorite">
-    Add to Favorites
-  </button>
+    <button
+        class="favorite"
+        v-if="!isFavorite(movie)"
+        @click="addToFavorites(movie)"
+    >
+        Add to favorites
+    </button>
+    <span v-else>
+        Favorite!
+        <a @click="true" class="remove_favorite">(UnFavorite)</a>
+    </span>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex';
+
 export default {
-  name: "FavoriteButton",
-  props: {
-    movie: Object
-  }
+    name: "FavoriteButton",
+    props: {
+        movie: Object
+    },
+    computed: mapState({
+        movies: state => state.movies,
+        favorites: state => state.favorites
+    }),
+    methods: {
+        ...mapActions(['addFavorite']),
+        addToFavorites: function ( movie ) {
+            console.log('adding: ' + movie);
+            this.addFavorite(movie);
+        },
+        isFavorite: function ( movie ) {
+            return this.favorites.filter(fave => fave.id == movie.id).length > 0;
+        }
+    }
 };
 </script>
 
